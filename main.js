72c09ac12a5edb0dcc21ca473d4cb0da1375a511
Cleidson Franklin <101678314+Cleidsondev@users.noreply.github.com>
2022-06-25 23:05:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const {width, height}  = container.getBoundingClientRect();
 container.addEventListener( 'mousemove', (event) => {
 const { offsetX, offsetY } = event;
 
-card.style.setProperty('--x-pos', (offsetX / width) - 0.6);
-card.style.setProperty('--y-pos', (offsetY / height) - 0.6);
+card.style.setProperty('--x-pos', (offsetX / width) - 0.4);
+card.style.setProperty('--y-pos', (offsetY / height) - 0.4);
 
 })
